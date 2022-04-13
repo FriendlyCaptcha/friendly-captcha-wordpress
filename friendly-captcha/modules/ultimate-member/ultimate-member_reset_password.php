b1c5d6d7073186b6221f22510bea69c19ad504d0
Florian Gareis <dev@zoker.me>
2022-04-13 13:46:51 +0200

    Fix ultimate member captcha verification

--- friendly-captcha/modules/ultimate-member/ultimate-member_reset_password.php
+++ friendly-captcha/modules/ultimate-member/ultimate-member_reset_password.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'um_after_password_reset_fields', 'frcaptcha_um_reset_password_show_widget', 10, 0 );
+add_action( 'um_after_password_reset_fields', 'frcaptcha_um_reset_password_show_widget', 500 );
 
 function frcaptcha_um_reset_password_show_widget() {
     $plugin = FriendlyCaptcha_Plugin::$instance;
@@ -16,32 +16,30 @@ function frcaptcha_um_reset_password_show_widget() {
     frcaptcha_enqueue_widget_scripts();
 }
 
-add_action( 'um_reset_password_errors_hook', 'frcaptcha_um_reset_password_validate', 20, 3 );	
+add_action( 'um_reset_password_errors_hook', 'frcaptcha_um_reset_password_validate', 20 );	
 
-function frcaptcha_um_reset_password_validate($args) {
+function frcaptcha_um_reset_password_validate($post) {
+    if ( isset( $post['mode'] ) && $post['mode'] == 'password') {
+        $plugin = FriendlyCaptcha_Plugin::$instance;
+        if (!$plugin->is_configured() or !$plugin->get_um_reset_password_active()) {
+            return;
+        }
 
-    if ( isset( $args['mode'] ) && ! $args['mode'] == 'password' && ! isset( $args['_social_login_form'] ) ) {
-		return;
-	}
+        $errorPrefix = '<strong>' . __( 'Error', 'frcaptcha' ) . '</strong> : ';
+        $solution = frcaptcha_get_sanitized_frcaptcha_solution_from_post();
+        
+        if ( empty( $solution ) ) {
+            $error_message = $errorPrefix . FriendlyCaptcha_Plugin::default_error_user_message() . __(' (captcha missing)', 'frcaptcha');
+            UM()->form()->add_error( 'frcaptcha', $error_message );
+            return;
+        }
 
-    $plugin = FriendlyCaptcha_Plugin::$instance;
-    if (!$plugin->is_configured() or !$plugin->get_um_reset_password_active()) {
-        return;
-    }
+        $verification = frcaptcha_verify_captcha_solution($solution, $plugin->get_sitekey(), $plugin->get_api_key());
 
-    $errorPrefix = '<strong>' . __( 'Error', 'frcaptcha' ) . '</strong> : ';
-	$solution = frcaptcha_get_sanitized_frcaptcha_solution_from_post();
-	
-	if ( empty( $solution ) ) {
-        $error_message = $errorPrefix . FriendlyCaptcha_Plugin::default_error_user_message() . __(' (captcha missing)', 'frcaptcha');
-        UM()->form()->add_error( 'frcaptcha', $error_message );
-		return;
-    }
-
-    $verification = frcaptcha_verify_captcha_solution($solution, $plugin->get_sitekey(), $plugin->get_api_key());
-
-    if (!$verification['success']) {
-        $error_message = $errorPrefix . FriendlyCaptcha_Plugin::default_error_user_message();
-        UM()->form()->add_error( 'frcaptcha', $error_message );
+        if (!$verification['success']) {
+            $error_message = $errorPrefix . FriendlyCaptcha_Plugin::default_error_user_message();
+            UM()->form()->add_error( 'frcaptcha', $error_message );
+            return;
+        }
     }
 }
--- friendly-captcha/modules/woocommerce/woocommerce_login.php
+++ friendly-captcha/modules/woocommerce/woocommerce_login.php
@@ -29,7 +29,7 @@ function frcaptcha_wc_login_validate($validation_error) {
         return;
     }
 
-    $errorPrefix = '<strong>' . __( 'Error', 'wp-captcha' ) . '</strong> : ';
+    $errorPrefix = '<strong>' . __( 'Error', 'wp-captcha' ) . '</strong>: ';
 	$solution = frcaptcha_get_sanitized_frcaptcha_solution_from_post();
 	
 	if ( empty( $solution ) ) {
