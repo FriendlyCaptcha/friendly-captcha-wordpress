b354d07a2d8120ed9ff369f2e27b0a51fa9bb3c7
merlinfuchs <merlinfuchs2001@gmail.com>
2023-06-26 21:36:12 +0200

    prepare 1.10.6

--- friendly-captcha/friendly-captcha.php
+++ friendly-captcha/friendly-captcha.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Friendly Captcha for WordPress
  * Description: Protect WordPress website forms from spam and abuse with Friendly Captcha, a privacy-first anti-bot solution.
- * Version: 1.10.5
+ * Version: 1.10.6
  * Requires at least: 5.0
  * Requires PHP: 7.3
  * Author: Friendly Captcha GmbH
@@ -19,7 +19,7 @@ if (!defined('WPINC')) {
 	die;
 }
 
-define('FRIENDLY_CAPTCHA_VERSION', '1.10.5');
+define('FRIENDLY_CAPTCHA_VERSION', '1.10.6');
 define('FRIENDLY_CAPTCHA_FRIENDLY_CHALLENGE_VERSION', '0.9.12');
 define('FRIENDLY_CAPTCHA_SUPPORTED_LANGUAGES', [
 	"en" => "English",
