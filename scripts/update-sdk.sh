8cd4f896eb30b5964718e498a40683db833ea05a
Merlin <merlinfuchs2001@gmail.com>
2024-04-19 13:50:12 +0200

    update friendly-captcha-sdk

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -1,6 +1,6 @@
 # V1
-curl -o ./friendly-captcha/public/vendor/v1/widget.module.min.js https://cdn.jsdelivr.net/npm/friendly-challenge@0.9.14/widget.module.min.js
-curl -o ./friendly-captcha/public/vendor/v1/widget.min.js https://cdn.jsdelivr.net/npm/friendly-challenge@0.9.14/widget.min.js
+curl -o ./friendly-captcha/public/vendor/v1/widget.module.min.js https://cdn.jsdelivr.net/npm/friendly-challenge/widget.module.min.js
+curl -o ./friendly-captcha/public/vendor/v1/widget.min.js https://cdn.jsdelivr.net/npm/friendly-challenge/widget.min.js
 
 # V2
 curl -o ./friendly-captcha/public/vendor/v2/site.min.js https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk/site.min.js
