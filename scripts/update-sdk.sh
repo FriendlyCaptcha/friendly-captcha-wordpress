e6d9cbafa15267bfdfdac1828ff284e5a7b02778
Guido Zuidhof <me@guido.io>
2024-11-28 14:23:23 +0100

    Update SDK to 0.1.10

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -4,6 +4,6 @@ curl -o ./friendly-captcha/public/vendor/v1/widget.module.min.js https://cdn.jsd
 curl -o ./friendly-captcha/public/vendor/v1/widget.polyfilled.min.js https://cdn.jsdelivr.net/npm/friendly-challenge@${V1_VERSION}/widget.polyfilled.min.js
 
 # V2
-V2_VERSION="0.1.8"
+V2_VERSION="0.1.10"
 curl -o ./friendly-captcha/public/vendor/v2/site.min.js https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk@${V2_VERSION}/site.min.js
 curl -o ./friendly-captcha/public/vendor/v2/site.compat.min.js https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk@${V2_VERSION}/site.compat.min.js
