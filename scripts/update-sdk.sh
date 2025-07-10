01cfe3db3229721186c14260c1bb763e8fb5c85b
merlinfuchs <merlinfuchs2001@gmail.com>
2025-07-10 13:04:56 +0200

    update js sdk

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -1,9 +1,9 @@
 # V1
-V1_VERSION="0.9.18"
+V1_VERSION="0.9.19"
 curl -o ./friendly-captcha/public/vendor/v1/widget.module.min.js https://cdn.jsdelivr.net/npm/friendly-challenge@${V1_VERSION}/widget.module.min.js
 curl -o ./friendly-captcha/public/vendor/v1/widget.polyfilled.min.js https://cdn.jsdelivr.net/npm/friendly-challenge@${V1_VERSION}/widget.polyfilled.min.js
 
 # V2
-V2_VERSION="0.1.10"
+V2_VERSION="0.1.25"
 curl -o ./friendly-captcha/public/vendor/v2/site.min.js https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk@${V2_VERSION}/site.min.js
 curl -o ./friendly-captcha/public/vendor/v2/site.compat.min.js https://cdn.jsdelivr.net/npm/@friendlycaptcha/sdk@${V2_VERSION}/site.compat.min.js
