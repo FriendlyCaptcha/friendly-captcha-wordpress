f9a6275cba8d9973b04792d42bc52d008a4c5164
Kilian Domaratius <kilian.domaratius@webneo.de>
2026-07-30 16:45:41 +0200

    Add support for elementor atomic forms

--- friendly-captcha/modules/elementor/elementor.php
+++ friendly-captcha/modules/elementor/elementor.php
@@ -27,5 +27,19 @@ function frcaptcha_elementor_add_form_field($form_fields_registrar)
     $form_fields_registrar->register(new \Elementor_Form_Friendlycaptcha_Field());
 }
 
+function frcaptcha_elementor_add_atomic_widget($widgets_manager): void
+{
+    $plugin = FriendlyCaptcha_Plugin::$instance;
+    if (!$plugin->is_configured()) {
+        return;
+    }
+
+    require_once(__DIR__ . "/atomic-widget.php");
+    $widgets_manager->register(new \Elementor_Friendlycaptcha_Atomic_Widget());
+}
+
+
 add_action('elementor/init', 'frcaptcha_elementor_init');
 add_action('elementor_pro/forms/fields/register', 'frcaptcha_elementor_add_form_field');
+add_action('elementor/widgets/register', 'frcaptcha_elementor_add_atomic_widget');
+
